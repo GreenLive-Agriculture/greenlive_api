86a2468c2c17700b62bc86abb783a41fad1c18f1
kouamdo <kouamdoux@gmail.com>
2023-09-20 19:15:41 +0100

    Initial commit API

---
+++ greenlive_api/src/main/java/osc/greenlive/GreenliveApiApplication.java
@@ -0,0 +1,13 @@
+package osc.greenlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenliveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenliveApiApplication.class, args);
+	}
+
+}
---
+++ greenlive_api/src/test/java/osc/greenlive/GreenliveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package osc.greenlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenliveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
